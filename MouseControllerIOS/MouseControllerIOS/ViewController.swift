1fd8126bdefcf69895d6e51ff8dc0922c9e28eb1
Rain_Li <36seed@gmail.com>
2018-05-16 23:13:27 -0400

    add button click and view swipe

--- MouseControllerIOS/MouseControllerIOS/ViewController.swift
+++ MouseControllerIOS/MouseControllerIOS/ViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet weak var scrollBar: UIView!
+    
+    var motionManager = CMMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // swipe gesture for scroll bar
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swipeScrollBar(gesture:)))
+        swipeUp.direction = UISwipeGestureRecognizerDirection.up
+        scrollBar.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swipeScrollBar(gesture:)))
+        swipeDown.direction = UISwipeGestureRecognizerDirection.down
+        scrollBar.addGestureRecognizer(swipeDown)
+        
+        // configure motionManager
+        motionManager.gyroUpdateInterval = 0.2
+        motionManager.startGyroUpdates(to: OperationQueue.current!) { (data, error) in
+            if let myData = data {
+                print(myData.rotationRate)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func clickLeftButton(_ sender: UIButton) {
+        print("left button clicked")
+    }
+    
+    @IBAction func clickRightButton(_ sender: UIButton) {
+        print("right button clicked")
+    }
+    
+    @objc func swipeScrollBar(gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.right:
+                    print("Swiped right")
+            case UISwipeGestureRecognizerDirection.down:
+                    print("Swiped down")
+            case UISwipeGestureRecognizerDirection.left:
+                    print("Swiped left")
+            case UISwipeGestureRecognizerDirection.up:
+                    print("Swiped up")
+            default:
+                break
+            }
+        }
+    }
 }
 
