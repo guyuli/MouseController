b1a3af546a5c679980de87c120ab2010a5dd3c12
Rain_Li <36seed@gmail.com>
2018-05-17 23:41:24 -0400

    add MouseControllerOSX

---
+++ MouseControllerOSX/MouseControllerOSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MouseControllerOSX
+//
+//  Created by Guyu Li on 2018-05-17.
+//  Copyright © 2018 Guyu Li. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MouseControllerOSX/MouseControllerOSX/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MouseControllerOSX
+//
+//  Created by Guyu Li on 2018-05-17.
+//  Copyright © 2018 Guyu Li. All rights reserved.
+//
+
+import Cocoa
+import MultipeerConnectivity
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
